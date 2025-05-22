9f9742f1f80fcaae5203c329e6c3573d2bc56533
KaueSiqueira <kaue54150@gmail.com>
2025-05-22 20:59:02 -0300

    Aula-sintaxe

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,31 @@
+﻿// using Fundamentos.Models;
+
+// string apresentacao = "Olá, seja bem vindo!";
+// int quantidade = 29;
+// double altura = 1.90;
+// decimal preco = 2.90M;
+// bool condicao = true;
+
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine(quantidade);
+// Console.WriteLine(altura);
+// Console.WriteLine(preco);
+// Console.WriteLine(condicao);
+
+// // Pessoa pessoa = new Pessoa();
+
+// // pessoa.Nome = "Kauê";
+// // pessoa.Idade = 20;
+
+// // pessoa.Apresentar();
+
+//Data atual do sistema
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+
+
