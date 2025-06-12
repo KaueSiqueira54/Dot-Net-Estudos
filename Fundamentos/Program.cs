7465f2eac58248bdf1122ef3c4cfb26444f44c36
KaueSiqueira <kaue54150@gmail.com>
2025-06-12 17:06:33 -0300

    estruturas de repetição

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -68,14 +68,94 @@
 //     break;
 // }
 
-bool ehMaiorDeIdade = false;
-bool altorizacao = false;
+using Fundamentos.Models;
 
-if (ehMaiorDeIdade || altorizacao)
+// bool ehMaiorDeIdade = false;
+// bool altorizacao = false;
+
+// if (ehMaiorDeIdade || altorizacao)
+// {
+//   Console.WriteLine("Entrada liberada.");
+// }
+// else
+// {
+//   Console.WriteLine("Entrada negada");
+// }
+
+// Calculadora calc = new Calculadora();
+
+// calc.Somar(10, 20);
+// calc.Subtrair(10, 20);
+// calc.Multiplicar(10, 20);
+// calc.Dividir(10, 20);
+// calc.Potencia(3, 3);
+// calc.Seno(30);
+// calc.Cos(30);
+// calc.Tan(30);
+// calc.RaizQuadeada(2);
+
+// int numero = 2;
+
+// for (int i = 0; i <= 10; i++)
+// {
+//   Console.WriteLine($"{numero} X {i} = {numero * i}");
+// }
+
+// int numero = 5;
+// int i = 0;
+
+// while (i <= 10)
+// {
+//   Console.WriteLine($"{numero} X {i} = {numero * i}");
+//   i++;
+// }
+
+// int soma = 0, numero = 0;
+
+
+// do
+// {
+//   Console.WriteLine("Digite um número: \n");
+//   numero = Convert.ToInt32(Console.ReadLine());
+//   soma += numero;
+// } while (numero != 0);
+
+// Console.WriteLine(soma);
+
+string opcao;
+bool exibir = true;
+
+while (exibir)
 {
-  Console.WriteLine("Entrada liberada.");
-}
-else
-{
-  Console.WriteLine("Entrada negada");
+  Console.WriteLine("Digite a sua opção:\n");
+  Console.WriteLine("1 - Cadastrar cliente\n");
+  Console.WriteLine("2 - Buscar cliente\n");
+  Console.WriteLine("3 - Apagar cliente\n");
+  Console.WriteLine("4 - Encerrar\n");
+
+  opcao = Console.ReadLine();
+
+  switch (opcao)
+  {
+    case "1":
+      Console.WriteLine("Cadastro de cliente");
+      break;
+
+    case "2":
+      Console.WriteLine("Busca de cliente");
+      break;
+
+    case "3":
+      Console.WriteLine("Apagar cliente");
+      break;
+
+    case "4":
+      Console.WriteLine("Encerrar");
+      exibir = false;
+      break;
+
+    default:
+      Console.WriteLine("Opção inválida!");
+      break;
+  }
 }
--- Fundamentos/obj/Debug/net8.0/Fundamentos.AssemblyInfo.cs
+++ Fundamentos/obj/Debug/net8.0/Fundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f9742f1f80fcaae5203c329e6c3573d2bc56533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a5b04fbdab027eadf1007b50d5e8cbd64a7aa33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
